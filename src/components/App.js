9ea42519974c709e07943b0e658e568d4deb3cc4
Michal Wasiak <michal.wasiak@hotmail.com>
2026-04-07 09:07:21 +1000

    Task 02 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-import Task01 from './../../01/Task01';
+
+
+
+// import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
@@ -8,10 +13,17 @@ import Task05 from './../../05/Task05';
 
 
 const App = () => {
+    const reducer = (state = { message: 'Dziala' }, action) => {
+        return state;
+    }
+    const store = createStore(reducer);
+
     return (
         <>
-            <Task01 />
-            {/* <Task02 /> */}
+            {/* <Task01 /> */}
+            <Provider store={store}>
+                <Task02 />
+            </Provider>
             {/* <Task03 /> */}
             {/* <Task04 /> */}
             {/* <Task05 /> */}
