78534c42ecada6e18a9fee134c38550dee021c9f
Michal Wasiak <michal.wasiak@hotmail.com>
2026-04-07 09:22:39 +1000

    Task 03 done

--- src/components/App.js
+++ src/components/App.js
@@ -2,32 +2,43 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-
-
-
-// import Task01 from './../../01/Task01';
+import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
+const initialState = {
+    message: 'Działa!',
+    time: new Date,
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'getCurrentTime':
+            return {
+                ...state,
+                time: new Date(),
+            };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
-    const reducer = (state = { message: 'Dziala' }, action) => {
-        return state;
-    }
-    const store = createStore(reducer);
-
     return (
-        <>
-            {/* <Task01 /> */}
-            <Provider store={store}>
-                <Task02 />
-            </Provider>
-            {/* <Task03 /> */}
+        <Provider store={store}>
+            <Task01 />
+            <Task02 />
+            <Task03 />
             {/* <Task04 /> */}
             {/* <Task05 /> */}
-        </>
+        </Provider>
     )
 }
 
